a1999e9b0c4b3ed6f1a25886ef900725708bd56a
Zubair Nabi <zn.zubairnabi@gmail.com>
2012-04-15 23:18:20 +0100

    SortMap and SortReduce ported to generic MapTask and ReduceTask classes

--- examples/src/main/java/com/asgow/ciel/examples/mapreduce/sort/SortMap.java
+++ examples/src/main/java/com/asgow/ciel/examples/mapreduce/sort/SortMap.java
@@ -1,74 +1,28 @@
 package com.asgow.ciel.examples.mapreduce.sort;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.util.StringTokenizer;
 
+import com.asgow.ciel.examples.mapreduce.common.MapTask;
 import com.asgow.ciel.examples.mapreduce.wordcount.Text;
-import com.asgow.ciel.executor.Ciel;
 import com.asgow.ciel.references.Reference;
-import com.asgow.ciel.references.WritableReference;
-import com.asgow.ciel.tasks.ConstantNumOutputsTask;
-import com.fasterxml.sort.SortConfig;
-import com.fasterxml.sort.std.TextFileSorter;
 
-public class SortMap implements ConstantNumOutputsTask {
+public class SortMap extends MapTask {
 
-    private Reference input;
-    private int nReducers;
-	
 	public SortMap(Reference input, int nReducers) {
-		this.input = input;
-		this.nReducers = nReducers;
-	}
-	
-	public int getNumOutputs() {
-		return this.nReducers;
-	}
-	
-	public Reference[] getDependencies() {
-		return new Reference[0];
+		super(input, nReducers);
+		// TODO Auto-generated constructor stub
 	}
 
-	public void invoke() throws Exception {
-        System.out.println("SortMap started at " + System.currentTimeMillis());
+	@Override
+	public void run(BufferedReader bufferedReader, DataOutputStream[] dos, int numReducers) {
         
-        // convert input reference to stream and then create a BufferedReader
-        InputStream inputStream = Ciel.RPC.getStreamForReference(this.input);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        
-        // number of output files would be equal to number of reducers, so creating that many outputstreams and references
-        OutputStream[] outputs = new OutputStream[nReducers];
-        WritableReference[] resultReference = new WritableReference[nReducers];
-        
-        // create temp files to store unsorted results
-        File tempFiles[] = new File[nReducers];
-        OutputStream[] tempOutputs = new OutputStream[nReducers];
-        DataOutputStream[] tempDos = new DataOutputStream[nReducers];
-        		       
-        for(int i = 0; i < nReducers; i++) {
-        	// create temp files and output streams
-        	tempFiles[i] = File.createTempFile("reduce_" + Integer.toString(i) , ".tmp");
-        	tempOutputs[i] = new FileOutputStream(tempFiles[i]);
-			tempDos[i] = new DataOutputStream(new BufferedOutputStream(tempOutputs[i]));
-			
-			// get references for output files and convert to OutputStream
-			resultReference[i] = Ciel.RPC.getOutputFilename(i);
-        	outputs[i] = resultReference[i].open();
-		}
-
         String line;
         Text blank = new Text("");
         try {
-			PartialHashOutputCollector<Text, Text> outMap = new PartialHashOutputCollector<Text, Text>(tempDos, nReducers, 1000);
+			PartialHashOutputCollector<Text, Text> outMap = new PartialHashOutputCollector<Text, Text>(dos, numReducers, 1000);
 			while ((line = bufferedReader.readLine()) != null) { 
 				//System.out.println(line);
 				StringTokenizer itr = new StringTokenizer(line);
@@ -79,32 +33,11 @@ public class SortMap implements ConstantNumOutputsTask {
 				}
 			}
 			outMap.flushAll();
-			
-			// now sort the temp files with 50 Mb mem limit
-			TextFileSorter sorter = new TextFileSorter(new SortConfig().withMaxMemoryUsage(50 * 1000 * 1000));
-			for(int i = 0; i < nReducers; i++) {
-				sorter.sort(new FileInputStream(tempFiles[i]), outputs[i]);
-		        // delete temp files
-		        tempFiles[i].delete();
-		        // close output stream
-		        outputs[i].close();
-			}
-			
-			// close output streams
-			for (DataOutputStream d : tempDos) 
-				d.close();
-
-		} catch (IOException e) {
+				
+        } catch (IOException e) {
 			System.out.println("IOException while running SortMap");
 			e.printStackTrace();
 			System.exit(1);
 		}
-
-        System.out.println("SortMap finished at " + System.currentTimeMillis());
 	}
-
-	public void setup() {
-
-	}
-
 }
--- examples/src/main/java/com/asgow/ciel/examples/mapreduce/sort/SortReduce.java
+++ examples/src/main/java/com/asgow/ciel/examples/mapreduce/sort/SortReduce.java
@@ -1,68 +1,24 @@
 package com.asgow.ciel.examples.mapreduce.sort;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.asgow.ciel.examples.mapreduce.common.MergeFiles;
+import com.asgow.ciel.examples.mapreduce.common.ReduceTask;
 import com.asgow.ciel.examples.mapreduce.wordcount.Text;
 
-import com.asgow.ciel.executor.Ciel;
 import com.asgow.ciel.references.Reference;
-import com.asgow.ciel.references.WritableReference;
-import com.asgow.ciel.tasks.ConstantNumOutputsTask;
 
-public class SortReduce implements ConstantNumOutputsTask {
-        
-    private Reference[] input;
-	
+public class SortReduce extends ReduceTask {
+    
 	public SortReduce(Reference[] input) {
-		this.input = input;
-	}
-	
-	public Reference[] getDependencies() {
-		return this.input;
-	}
-	
-	public int getNumOutputs() {
-		return 1;
+		super(input);
+		// TODO Auto-generated constructor stub
 	}
 
-	public void invoke() throws Exception {
-        System.out.println("SortReduce started at " + System.currentTimeMillis());
-        int nInputs = input.length;
-		InputStream[] is = new InputStream[nInputs];
-		DataOutputStream[] dos = new DataOutputStream[1];
-        OutputStream[] outputs = new OutputStream[1];
-        List<InputStream> listStreams = new ArrayList<InputStream>();
-		
-		for(int i = 0; i < nInputs; i++) {
-			is[i] = new BufferedInputStream(Ciel.RPC.getStreamForReference(this.input[i]));
-			listStreams.add(i, is[i]);
-		}
-        
-        File tempFile = File.createTempFile("reduce_" + Integer.toString(nInputs) , ".tmp");
-        FileOutputStream tempOutput = new FileOutputStream(tempFile);
-        
-        MergeFiles mergeFiles = new MergeFiles();
-        mergeFiles.mergeFiles(listStreams, tempOutput);
-				
-		WritableReference resultReference = Ciel.RPC.getOutputFilename(0);	
-        outputs[0] = resultReference.open();
-		dos[0] = new DataOutputStream(new BufferedOutputStream(outputs[0]));
-		
-        DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(tempFile)));
-
+	@Override
+	public void run(DataOutputStream[] dos, DataInputStream dis) {
 		try {
 			PartialHashOutputCollector<Text, Text> outMap = new PartialHashOutputCollector<Text, Text>(dos, 1, Integer.MAX_VALUE);
 			Text word = new Text();
@@ -83,20 +39,13 @@ public class SortReduce implements ConstantNumOutputsTask {
 			
 			//flush all key, values to collector, close the data stream, and delete the temp file
 			outMap.flushAll();
-			dos[0].close();			
-			tempFile.delete();
-			
+						
 		} catch (IOException e) {
 			System.out.println("IOException while running SortReduce");
 			e.printStackTrace();
 			System.exit(1);
 		}
-
-        System.out.println("SortReduce finished at " + System.currentTimeMillis());		
-	}
-
-	public void setup() {
-		
+	
 	}
 
 }
