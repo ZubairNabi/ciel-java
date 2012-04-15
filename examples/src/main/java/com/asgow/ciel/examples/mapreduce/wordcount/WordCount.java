e0906d6cf6d683f18496c7e0d0e234987682f0ca
ZubairNabi <zn.zubairnabi@gmail.com>
2012-04-15 18:30:40 +0100

    getReferencesFromPackage method used

--- examples/src/main/java/com/asgow/ciel/examples/mapreduce/wordcount/WordCount.java
+++ examples/src/main/java/com/asgow/ciel/examples/mapreduce/wordcount/WordCount.java
@@ -1,7 +1,5 @@
 package com.asgow.ciel.examples.mapreduce.wordcount;
 
-import java.io.IOException;
-
 import com.asgow.ciel.executor.Ciel;
 import com.asgow.ciel.references.Reference;
 import com.asgow.ciel.tasks.FirstClassJavaTask;
@@ -11,28 +9,25 @@ public class WordCount implements FirstClassJavaTask {
 
 	public void invoke() throws Exception {
 		// check args
-        if(Ciel.args.length != 2) {
-        	Ciel.returnPlainString("Invalid number of arguments");
+        if(Ciel.args.length != 3) {
+        	Ciel.returnPlainString("Invalid number of arguments. Usage: com.asgow.ciel.examples.mapreduce.wordcount.Wordcount" +
+        			" [NUM_OF_REDUCE_TASKS] [NUM_OF_INPUTS] -P input[n]=[PATH_TO_FILE], where 0 =< n < total_inputs");
         } 
-        // get number of map and reduce tasks
-        int numMaps = Integer.parseInt(Ciel.args[0]);
-        int numReduces = Integer.parseInt(Ciel.args[1]);
-    	
-        // get input references
-        Reference[] mapInputs = new Reference[numMaps]; 
-        //TODO: Right now all maps are assigned the same input file, change to assign each a different one
-        for (int i = 0; i < numMaps; ++i) {
-        	mapInputs[i] = Ciel.RPC.packageLookup("input" + Integer.toString(i));
-		}
-        
+        // get number of reduce tasks and input files
+        int numReduces = Integer.parseInt(Ciel.args[0]);
+        int numInputs = Integer.parseInt(Ciel.args[1]);
+              
         // create MapReduce object
         MapReduce mapReduce = new MapReduce();
+    	
+        // get input references
+        Reference[] mapInputs = mapReduce.getReferencesFromPackage(numInputs);
         
         // create maps
-        Reference[][] mapResults = mapReduce.map("com.asgow.ciel.examples.mapreduce.wordcount.WordCountMap", mapInputs, numMaps, numReduces);
+        Reference[][] mapResults = mapReduce.map("com.asgow.ciel.examples.mapreduce.wordcount.WordCountMap", mapInputs, numInputs, numReduces);
         
 		// now shuffle map outputs so that each reduce task receives an input file from each map
-		Reference[][] reduceInput = mapReduce.shuffle(mapResults, numMaps, numReduces);
+		Reference[][] reduceInput = mapReduce.shuffle(mapResults, numInputs, numReduces);
 		
 		mapReduce.reduce("com.asgow.ciel.examples.mapreduce.wordcount.WordCountReduce", reduceInput, numReduces);
 	}
