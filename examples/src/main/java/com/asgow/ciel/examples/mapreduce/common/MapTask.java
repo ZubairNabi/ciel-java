5954c9a26e22cb3607c7d215120ab1c1e40f0593
ZubairNabi <zn.zubairnabi@gmail.com>
2012-04-16 10:21:43 +0100

    redundant imports removed

--- examples/src/main/java/com/asgow/ciel/examples/mapreduce/common/MapTask.java
+++ examples/src/main/java/com/asgow/ciel/examples/mapreduce/common/MapTask.java
@@ -6,13 +6,10 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.StringTokenizer;
 
-import com.asgow.ciel.examples.mapreduce.wordcount.Text;
 import com.asgow.ciel.executor.Ciel;
 import com.asgow.ciel.references.Reference;
 import com.asgow.ciel.references.WritableReference;
