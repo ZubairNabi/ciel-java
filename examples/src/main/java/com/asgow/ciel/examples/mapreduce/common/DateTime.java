0143fcca73cbb08518d6b5451189fdea40228061
Zubair Nabi <zn.zubairnabi@gmail.com>
2012-04-29 12:39:05 +0100

    DateTime class made serializable

--- examples/src/main/java/com/asgow/ciel/examples/mapreduce/common/DateTime.java
+++ examples/src/main/java/com/asgow/ciel/examples/mapreduce/common/DateTime.java
@@ -1,11 +1,16 @@
 package com.asgow.ciel.examples.mapreduce.common;
 
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DateTime {
+public class DateTime implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2052568223631842589L;
 	private DateFormat dateFormat;
 	private Date date;
 	
